8a02be9468624add66e76ec4f7045c0f61b69249
Dirganhere <98895216+EkooNihh@users.noreply.github.com>
2022-12-18 14:48:07 +0700

    Update __init__.py

--- assistant/__init__.py
+++ assistant/__init__.py
@@ -1,9 +1,9 @@
 # Ultroid - UserBot
 # Copyright (C) 2021-2022 TeamUltroid
 #
-# This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
+# This file is a part of < https://github.com/EkooNihh/Dirgan-Userbot/ >
 # PLease read the GNU Affero General Public License in
-# <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
+# <https://github.com/EkooNihh/Dirgan-Userbot/blob/main/LICENSE/>.
 
 from telethon import Button, custom
 
